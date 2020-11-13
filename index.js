2b0c65fa1f928053bce20c8eaf0d24ecb10c60bf
robin <robin>
2020-11-13 20:15:46 +0100

    added morgan

--- index.js
+++ index.js
@@ -5,10 +5,14 @@
 
 // ENV
 const PORT = process.env.CRISTAL_PORT;
+const MORGAN_LEVEL = process.env.CRISTAL_DB_CRISTAL_DB_LOGLEVEL;
 
 // Express server
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
 // Routes
 const tokenRequest = require("./routes/tokenRequest");
@@ -19,6 +23,15 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "log", "access.log"),
+    { flags: "a" }
+);
+
+// Morgan logger
+app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/auth", tokenRequest);
 app.use("/system", systemRoute);
